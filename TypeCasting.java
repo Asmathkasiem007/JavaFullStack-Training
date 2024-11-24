482cdd4df74e2df23d21b010cb65575a037246b6
Asmathdillfar K <asmathdillfark@gmail.com>
2024-11-24 21:56:25 +0530

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,10 @@
+public class TypeCasting {
+	public static void main(String[] args) {
+		int a=5;
+		float f=a;
+		double d=f;
+		float x=(float)d;
+		System.out.print("A : "+a+" F : "+f+" D : "+d+" X : "+x);
+	}
+}
+	
